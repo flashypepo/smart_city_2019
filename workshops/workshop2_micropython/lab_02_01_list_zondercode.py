05bcfdc03cbe22d4dd11d3cb226a52a15b164293
Peter <peter@pepo.nl>
2019-09-12 20:53:06 +0200

    workshop 2019-0913
    opdracht lab-oefening 1 van workshop 2

---
+++ workshops/workshop2_micropython/lab_02_01_list_zondercode.py
@@ -0,0 +1,30 @@
+"""
+lab-oefening: temperaturen inlezen uit een bestand en
+basisinformatie van de temperaturen tonen.
+
+In deze opdracht ga je temperaturen inlezen uit een bestand
+en de volgende basisinformatie op het console tonen:
+* de hoogste en laagste temperatuur,
+* de gemiddelde temperatuur,
+* en de mediane temperatuur.
+
+De temperaturen zijn de maandelijks hoogste temperaturen op
+Heathrow Airport van 1948 tot en met 2016.
+Vervolgens haal je wat basisinformatie uit de temperaturen:
+
+De mediane temperatuur is 'de temperatuur in het midden van
+de lijst' als alle temperaturen zijn gesorteerd.
+
+Gegevens en tips:
+Bestand: 'lab_temp.txt'
+Maak gebruik van data-type list voor de temperaturen.
+Maak gebruik van de string functie 'strip()'.
+"""
+# lees temperaturen uit bestand lab_temp.txt
+# en in een list-variabele temperatures
+# open de file,
+# lees regel-voor-regel de temperatuur uit de file,
+# voegtoe de temperatuur als float-number aan de list-variabele temperatures
+#
+# bepaal minimum, maximum, gemiddelde en mediane temperaturen
+# en toon op scherm/console
