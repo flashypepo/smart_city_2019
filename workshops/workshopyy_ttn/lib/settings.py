66a98bf28b646a129473827865beac348edfb245
Peter <p.vander.post@windesheim.nl>
2019-12-03 17:13:34 +0100

    Delete settings.py

--- workshops/workshopyy_ttn/lib/settings.py
+++
@@ -1,7 +0,0 @@
-# settings.py
-# 2019-0202 device 'pepolopy4wf'
-import binascii
-
-# The Application End Device Unique Identifier
-app_eui = binascii.unhexlify('70B3D57ED000BF9B')
-app_key = binascii.unhexlify('0BD766F6D48A49C305D01CB373D1FE9F')
